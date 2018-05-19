6cbbf65adb7988176f4bf5e1bb39c39eba9491c6
neo <neoip.0514@gmail.com>
2018-05-19 16:23:39 +0900

    spring-boot project apply

---
+++ multi-frontend/src/main/java/com/test/multi/multifrontend/MultiFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.test.multi.multifrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiFrontendApplication.class, args);
+	}
+}
