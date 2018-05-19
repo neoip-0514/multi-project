0932d711a8040adde256d5bac63e7dc73f3160d2
neo <neoip.0514@gmail.com>
2018-05-19 16:30:10 +0900

    apply spring-boot to multi-api

---
+++ multi-api/src/main/java/com/test/multi/multiapi/MultiApiApplication.java
@@ -0,0 +1,12 @@
+package com.test.multi.multiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiApiApplication.class, args);
+	}
+}
